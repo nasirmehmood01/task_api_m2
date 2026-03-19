c0e46f71cc8b1b4d80b6313222b3815ccde2a1a2
Nasir Mehmood <nasirmalik1771@gmail.com>
2026-03-19 13:38:54 +0100

    Update server.js

--- server.js
+++ server.js
@@ -16,10 +16,13 @@ app.use(express.json());
 const PORT = process.env.PORT || 3000;
 const APP_NAME = process.env.APP_NAME || "Task API";
 const NODE_ENV = process.env.NODE_ENV || "development";
+const NODE_ENV = process.env.USER || "Unknown";
 
 console.log("Starting server...");
+console.log("PORT:", PORT);
 console.log("Environment:", NODE_ENV);
-
+console.log("Task API:", APP_NAME);
+console.log("USER:", NODE_ENV);
 /* =========================
    IN-MEMORY DATA
 ========================= */
